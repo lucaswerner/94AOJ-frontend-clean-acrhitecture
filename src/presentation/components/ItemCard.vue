38261154083481f051506f0788a40453ae77594a
Lucas Werner <lucaswerner26@gmail.com>
2025-01-13 22:25:16 -0300

    item card

---
+++ src/presentation/components/ItemCard.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+type ItemData = {
+    id: number,
+    image: string,
+    title: string,
+    description: string
+    value: number
+}
+
+defineProps<{
+    data: ItemData,
+    onAdd: (data: ItemData) => void,
+    onRemove: (data: ItemData) => void
+    count: number
+}>()
+
+function formatNumber(numberValue: number = 0) {
+    return numberValue.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+}
+</script>
+
+<template>
+    <v-hover v-slot="{ isHovering, props }">
+        <v-card variant="tonal" v-bind="props" :elevation="isHovering ? 24 : 6" class="item-card-wrapper">
+            <v-row>
+                <v-col>
+                    <v-img :src="data.image"></v-img>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col>
+                    <v-card-title>{{ data.title }}</v-card-title>
+                    <v-card-text>{{ data.description }}</v-card-text>
+                </v-col>
+            </v-row>
+            <v-card-actions>
+                <div class="price-wrapper">
+                    <label class="currency">R$</label>
+                    <span class="price">{{ formatNumber(data.value) }}</span>
+                </div>
+
+                <div class="option-wrapper">
+                    <v-btn icon="mdi-minus-circle" @click="onRemove(data)"></v-btn>
+                    <span class="count">{{ count }}</span>
+                    <v-btn icon="mdi-plus-circle" @click="onAdd(data)"></v-btn>
+                </div>
+            </v-card-actions>
+        </v-card>
+    </v-hover>
+</template>
+
+<style scoped>
+.item-card-wrapper {
+    max-width: 300px;
+}
+
+.v-card-actions {
+    display: flex;
+    justify-content: space-between;
+}
+
+.price-wrapper .currency {
+    font-size: 15px;
+    font-family: Roboto, sans-serif;
+    margin-right: 5px;
+}
+
+.price-wrapper .price {
+    font-size: 20px;
+    font-weight: 800;
+}
+
+.option-wrapper .count {
+    padding: 0 15px;
+}
+</style>
