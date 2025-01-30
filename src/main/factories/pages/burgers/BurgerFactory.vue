2b36031fe35aeecb5247687a1fb0abd73a0a3ec9
Laboratório FIAP <labsfiap@fiap.com.br>
2025-01-29 22:10:14 -0300

    connecting to burgers api

--- src/main/factories/pages/burgers/BurgerFactory.vue
+++ src/main/factories/pages/burgers/BurgerFactory.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
-import { cardDataList } from '../../../../data/burgerData';
+import { onMounted, reactive } from 'vue';
 import { CardData } from '../../../../domain/models/cardData';
 import { OrderType } from '../../../../domain/models/orderType';
 import CardContainer from '../../../../presentation/components/CardContainer.vue';
 import { MakeOrderAdapter } from '../../cache/orderAdapterFactory';
+import { MakeHamburgersQuery } from '../../usecases/HamburgersFactory';
 
 const orderAdapter = MakeOrderAdapter();
 const onAdd = (data: CardData) => orderAdapter.add(OrderType.HAMBURGERS, data);
+
+const state: { posts: Array<CardData> } = reactive({
+  posts: []
+})
+
+onMounted(async () => {
+  try {
+    const response = await MakeHamburgersQuery().get();
+    state.posts = response.map(burger => ({ ...burger, value: burger.values.combo }));
+  } catch (error) {
+    console.error('Error fetching jobs:', error);
+  }
+});
 </script>
 
 <template>
-  <CardContainer v-bind:card-list="cardDataList" v-on:add="onAdd" />
+  <CardContainer v-bind:card-list="state.posts" v-on:add="onAdd" />
 </template>
 
 <style scoped></style>
--- src/main/routes/apiRoutes.ts
+++ src/main/routes/apiRoutes.ts
@@ -2,5 +2,6 @@ const baseUrl = "https://burgerlivery-api.vercel.app";
 
 export const apiRoutes = {
   login: `${baseUrl}/user/login`,
+  hamburgers: `${baseUrl}/hamburgers`,
   orders: `${baseUrl}/orders`,
 };
